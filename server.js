5e56ce746b956dd75b7cfc4f15aa7da71d1d74ee
unknown <wei.xw.xu@gmail.com>
2019-12-28 17:11:25 -0500

    bug fix

--- server.js
+++ server.js
@@ -17,18 +17,6 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
-//serve static asset if in production
-if(process.env.NODE_ENV === 'production'){
-  console.log("Production environment. Serving static asset from client/build.");
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  // server will serve up index.html in the build folder if in production
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
-
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
@@ -71,5 +59,17 @@ io.on('connect', (socket) => {
     }
   })
 });
+
+//serve static asset if in production
+if(process.env.NODE_ENV === 'production'){
+  console.log("Production environment. Serving static asset from client/build.");
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // server will serve up index.html in the build folder if in production
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 server.listen(port, () => console.log(`Server has started on port ${port}`));
