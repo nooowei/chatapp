450df0670215c5bb4f77fa706cb080c33d4bbb06
unknown <wei.xw.xu@gmail.com>
2019-12-28 16:57:47 -0500

    bug fix

--- server.js
+++ server.js
@@ -72,4 +72,4 @@ io.on('connect', (socket) => {
   })
 });
 
-app.listen(port, () => console.log(`Server has started on port ${PORT}`));
+app.listen(port, () => console.log(`Server has started on port ${port}`));
