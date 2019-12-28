23d93b5b58e7d91feb872e2854c80ed49f75ba0a
unknown <wei.xw.xu@gmail.com>
2019-12-28 18:04:41 -0500

    bug fix

--- router.js
+++ router.js
@@ -2,20 +2,24 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 
-//serve static asset if in production
-if(process.env.NODE_ENV === 'production'){
-  console.log("Production environment. Serving static asset from client/build.");
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  // server will serve up index.html in the build folder if in production
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+// //serve static asset if in production
+// if(process.env.NODE_ENV === 'production'){
+//   console.log("Production environment. Serving static asset from client/build.");
+//   // Set static folder
+//   app.use(express.static('client/build'));
+//
+//   // server will serve up index.html in the build folder if in production
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
+//
+// router.get("/", (req, res) => {
+//   res.send({ response: "Server is up and running." }).status(200);
+// });
 
 router.get("/", (req, res) => {
-  res.send({ response: "Server is up and running." }).status(200);
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 
